8d828aafa2c47afc6411138ceeda9ab91a4e301a
Nader Almogazy <nader73107@gmail.com>
2022-01-19 19:02:02 -0500

    Co-authored-by: Leo Crossman <leocrossman@users.noreply.github.com> Co-authored-by: ep1815 <ep1815@users.noreply.github.com>

--- Qachengo/Qachengo.js
+++ Qachengo/Qachengo.js
@@ -1,5 +1,5 @@
 class Cache {
-  constructor(options = {expiration: 1000 * 60 * 10}) {
+  constructor(options = { expiration: 1000 * 60 * 10 }) {
     //set expiration in options or default to 10min
     this.defaultExpiration = options.expiration; //10 minute default expiration
     //Prevents excessive cleanups
@@ -8,36 +8,64 @@ class Cache {
     this.content = {}; // STORE OF DATA
   }
 
+  _getRefs(info) {
+    return this.parseQuery(info.operation.selectionSet.selections);
+  }
+
+  _storeData(fields, dbResponse) {
+    const isObject = (x) => typeof x === 'object' && x !== null;
+
+    for (let field in dbResponse) {
+      if (isObject(dbResponse[field])) {
+        this._storeData(fields, dbResponse[field]);
+      } else {
+        this.content[field] = {
+          value: dbResponse[field],
+          expires: Date.now() + this.defaultExpiration,
+        };
+      }
+    }
+  }
+
   // Set data into Cache
-  set(key, value) {
-    //cleans stale data before all
-    this.cleanUp(key);
-
-    if (this.content[key]) {
-      //Overwrite old data, resets expiration date... valuable for frequently used keys
-      this.content[key] = {
-        value,
-        expires: Date.now() + this.defaultExpiration,
-      };
-    } else {
-      //Create new key, set expiration date, based  off current date + expiration value
-      this.content[key] = {
-        value,
-        expires: Date.now() + this.defaultExpiration,
-      };
-    }
+  set(info, dbResponse) {
+    const { fields } = this._getRefs(info);
+    this._storeData(fields, dbResponse);
   }
+
   //Check for key, and return value if found
-  check(key) {
-    //cleans stale data before all
-    this.cleanUp(key);
+  check(info) {
+    const { selections, fields, queryObj } = this._getRefs(info);
 
-    if (this.content[key]) {
-      return this.content[key].value;
-    } else {
-      return null;
+    let isMissingData = false;
+    for (const field of fields) {
+      //cleans stale data out of corresponding field
+      this.cleanUp(field);
+
+      if (this.content.hasOwnProperty(field)) {
+        this._addToQueryObj(field, this.content[field].value, queryObj);
+      } else {
+        isMissingData = true;
+        break;
+      }
+    }
+
+    return isMissingData ? null : queryObj;
+  }
+
+  _addToQueryObj(field, value, queryObj) {
+    const isObject = (x) => typeof x === 'object' && x !== null;
+
+    for (let key in queryObj) {
+      if (isObject(queryObj[key])) {
+        this._addToQueryObj(field, value, queryObj[key]);
+      } else if (queryObj[key] === field) {
+        queryObj[key] = value;
+        return;
+      }
     }
   }
+
   //Cleans up stale data
   cleanUp(key) {
     //Evict a stale key if key is provided
@@ -45,20 +73,60 @@ class Cache {
       if (this.content[key].expires < Date.now()) {
         delete this.content[key];
       }
-    } 
+    }
     //Option to cleanUp all keys if need arises
     else {
-      for(const key in this.content){
-        if(this.content[key].expires < Date.now()){
-          delete this.content[key]
+      for (const key in this.content) {
+        if (this.content[key].expires < Date.now()) {
+          delete this.content[key];
         }
       }
     }
   }
   //count amount of keys
-  size(){
-    return Object.keys(this.content).length
+  size() {
+    return Object.keys(this.content).length;
+  }
+
+  /**
+   * @param {object[]} selectionSet
+   * @return {object} fields
+   */
+  parseQuery(selections, isTopLevel = true) {
+    //Create a fields array to hold all REQUESTED FIELDS from GQL QUERY
+    const fields = [];
+
+    //Create a queryObj, to be the template for our return Obj
+    const queryObj = {};
+
+    // Loop through GraphQL field selections
+    for (const selection of selections) {
+      //if current field selection has subFields
+      if (selection.selectionSet !== undefined) {
+        //Parse through subfields, and deconstruct the fields and queryObj
+        const result = this.parseQuery(
+          selection.selectionSet.selections,
+          false
+        );
+        const key = selection.name.value;
+        //add subfields to final queryObj and Fields
+        queryObj[key] = result.queryObj;
+        fields.push(...result.fields);
+      } else {
+        // no selection set, so must be basic primitive field
+        // Add to fields and queryObj
+        const key = selection.name.value;
+        queryObj[key] = selection.name.value;
+        fields.push(selection.name.value);
+      }
+    }
+    // if we are in the inital call before any recursion, we remove the top of the property associated with the returning query object to match the response from the database.
+    const topLevelField = selections[0].name.value;
+    //The topLevel Object is expected to not be included, so we add logic to omit
+    return isTopLevel
+      ? { fields, queryObj: queryObj[topLevelField] }
+      : { fields, queryObj };
   }
 }
 
-module.exports = Cache
+module.exports = Cache;
