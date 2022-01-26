4e500af7c27d285d34372728e569b11adf933de0
Nader Almogazy <nader73107@gmail.com>
2022-01-26 10:15:11 -0500

    supressed TS implicit any type on index

--- demo-app/server/resolvers/resolvers.js
+++ demo-app/server/resolvers/resolvers.js
@@ -90,21 +90,3 @@ module.exports = {
     return;
   }
 };
-//
-// getAllProducts = async (req, res, next) => {
-//   try {
-//     const data = await Product.find({});
-//     console.log('All products: ', data);
-//     res.locals.allProducts = data;
-//     return next();
-//   } catch (error) {
-//     res.status(400).send({ error: error.message })
-//   }
-// },
-// getProduct = async (req, res, next) => {
-//   try {
-
-//   } catch (error) {
-//     res.status(400).send({ error: error.message })
-//   }
-// },
