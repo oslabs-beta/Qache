0cdc364fcae4d34730b44906eb1031a0e66bfa34
leocrossman <lkcrossman@gmail.com>
2022-02-07 12:32:25 -0500

    added accessCount property to Node class test. added test for adding a node to an LRU cache of size 1. added a user2 test array of objects (users) to test list additions

--- __tests__/Qache.test.js
+++ __tests__/Qache.test.js
@@ -34,12 +34,13 @@ describe('Qache Tests', () => {
     beforeEach(() => {
       node = new Node('testKey', { testData: 'testVal' });
     });
-    it('should have properties: keyRef, value, prev, next, expires', () => {
+    it('should have properties: keyRef, value, prev, next, expires, accessCount', () => {
       expect(node.keyRef).toBeDefined();
       expect(node.value).toBeDefined();
       expect(node.prev).toBeDefined();
       expect(node.next).toBeDefined();
       expect(node.expires).toBeDefined();
+      expect(node.accessCount).toBeDefined();
     });
   });
 
@@ -48,7 +49,9 @@ describe('Qache Tests', () => {
     // this beforeEach will change soonish... will be replaced with whatever's relevant or just removed.
     beforeEach(() => {
       users = [...testUsers];
+      users2 = [...testUsers].map((user, idx) => user.username + idx);
       userNode = new Node('users', users);
+      userNode2 = new Node('users2', users2);
       cache = new Cache();
     });
 
@@ -109,7 +112,7 @@ describe('Qache Tests', () => {
         let node = cache.content[key];
         expect(node).toBeInstanceOf(Node);
         expect(node).toEqual(userNode);
-        expect(node.keyRef).toBe('users');
+        expect(node.keyRef).toBe(key);
         expect(node.value).toBe(users);
         expect(node.prev).toBe(null);
         expect(node.next).toBe(null);
@@ -119,25 +122,28 @@ describe('Qache Tests', () => {
       });
     });
 
-    describe('LRU', () => {
-      xdescribe('get() (LRU specific)', () => {
+    describe('LRU Eviction Policy Tests:', () => {
+      xdescribe('get()', () => {
         xit('should add a node to the queue and cache when there is one element in the cache', () => {});
         xit('should add a node to the queue and cache when there are two elements in the cache', () => {});
         xit('should add a node to the queue and cache when there are more than two elements in the cache', () => {});
       });
-      describe('set() (LRU specific)', () => {
-        it('should add a node to the queue and cache when there is one element in the cache', () => {
+      describe('set()', () => {
+        it('should add a node to the queue and cache when there is one element in the cache and update the cache tail to point to the added node', () => {
           cache.set('users', users);
+          cache.set('users2', users2);
+          userNode.next = userNode2;
+          userNode2.prev = userNode;
           expect(cache.head).toEqual(userNode);
-          expect(cache.tail).toEqual(userNode);
-          expect(cache.content['users']).toEqual(userNode);
+          expect(cache.tail).toEqual(userNode2);
+          expect(cache.tail).toBeInstanceOf(Node);
         });
         xit('should add a node to the queue and cache when there are two elements in the cache', () => {});
         xit('should add a node to the queue and cache when there are more than two elements in the cache', () => {});
       });
     });
-    describe('LFU', () => {
-      it('should', () => {});
+    describe('LFU Eviction Policy Tests:', () => {
+      it('should ', () => {});
     });
   });
 });
